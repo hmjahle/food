edfe92f243b1a507436c144ff2522a344eedce7c
hmjahle <hansmarkusjahle@gmail.com>
2019-10-27 18:42:34 +0100

    finished the seach bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+
+import Feather from 'react-native-vector-icons/Feather';
+
+
+const SearchBar = ({term, onTermChange, onTermSubmitt}) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="search" 
+                style={styles.inputStyle}
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
+                onEndEditing={()=>onTermSubmitt(console.log('submitted'))}
+                />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 15,
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15    
+    }
+});
+
+export default SearchBar;
